8471ef0739ef4c6c6c2ff9b04de5a3a94760a3a7
QuincyClain <pesotskiyvlad9@gmail.com>
2023-09-14 22:45:55 +0300

    initial commit

---
+++ LR1.1/LR1.1.XUnitTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace LR1._1.XUnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestPrintHelloWorld()
+    {
+        using (StringWriter sw = new StringWriter())
+        {
+            Console.SetOut(sw); // Перехват вывода в консоль
+            Program.PrintHelloWorld();
+            string printedOutput = sw.ToString().Trim();
+            string expectedOutput = "Hello, world!\nAndhiagain!";
+
+            Assert.Equal(expectedOutput, printedOutput);
+        }
+    }
+
+    [Fact]
+    public void TestPrintRandomExclamationMarks()
+    {
+        using (StringWriter sw = new StringWriter())
+        {
+            Console.SetOut(sw); // Перехват вывода в консоль
+            Program.PrintRandomExclamationMarks();
+            string printedOutput = sw.ToString().Trim();
+
+            Assert.NotEmpty(printedOutput);
+            Assert.Matches("^!{5,50}$", printedOutput); // Проверка количества восклицательных знаков
+        }
+    }
+}
---
+++ LR1.1/LR1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LR1._1;
+public class Program
+{
+    static void Main()
+    {
+        PrintHelloWorld();
+        PrintRandomExclamationMarks();
+    }
+
+    public static void PrintHelloWorld()
+    {
+        Console.WriteLine("Hello, world!");
+        Console.WriteLine("Andhiagain!");
+    }
+
+    public static void PrintRandomExclamationMarks()
+    {
+        Random random = new Random();
+        int exclamationCount = random.Next(5, 51);
+
+        string exclamationMarks = new string('!', exclamationCount);
+        Console.WriteLine(exclamationMarks);
+    }
+}
+

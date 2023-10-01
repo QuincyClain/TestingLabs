d4ee5413ddf38e6d68ffba8f6e12cdcd97911993
QuincyClain <pesotskiyvlad9@gmail.com>
2023-10-01 16:47:02 +0300

    task 3 complete

---
+++ LR1.3/LR1.3.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace LR1._3.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestCalculateRectangleArea_ValidInput()
+    {
+        // Arrange
+        double length = 5;
+        double width = 3;
+
+        // Act
+        double area = Program.CalculateRectangleArea(length, width);
+
+        // Assert
+        Assert.Equal(15, area);
+    }
+
+    [Fact]
+    public void TestCalculateRectangleArea_NegativeInput()
+    {
+        // Arrange
+        double length = -5;
+        double width = 3;
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => Program.CalculateRectangleArea(length, width));
+    }
+
+    [Fact]
+    public void TestCalculateRectangleArea_ZeroInput()
+    {
+        // Arrange
+        double length = 0;
+        double width = 3;
+
+        // Act
+        double area = Program.CalculateRectangleArea(length, width);
+
+        // Assert
+        Assert.Equal(0, area);
+    }
+}
---
+++ LR1.3/LR1.3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace LR1._3;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        double length, width;
+
+        while (true)
+        {
+            Console.Write("Введите длину прямоугольника: ");
+            if (double.TryParse(Console.ReadLine(), out length) && length > 0)
+                break;
+            Console.WriteLine("Некорректное значение. Длина должна быть положительным числом.");
+        }
+
+        while (true)
+        {
+            Console.Write("Введите ширину прямоугольника: ");
+            if (double.TryParse(Console.ReadLine(), out width) && width > 0)
+                break;
+            Console.WriteLine("Некорректное значение. Ширина должна быть положительным числом.");
+        }
+
+        double area = CalculateRectangleArea(length, width);
+        Console.WriteLine($"Площадь прямоугольника: {area}");
+    }
+
+    public static double CalculateRectangleArea(double length, double width)
+    {
+        if (length < 0 || width < 0)
+        {
+            throw new ArgumentException("Длина и ширина прямоугольника должны быть положительными числами.");
+        }
+
+        return length * width;
+    }
+}
+

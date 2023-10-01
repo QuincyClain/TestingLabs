4bd4d0f67567c07f628bcb38ed6a0c48a91b40ec
QuincyClain <pesotskiyvlad9@gmail.com>
2023-10-01 18:32:47 +0300

    task 6 complete

---
+++ LR1.6/LR1.6.Tests/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace LR1._6.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public async void TestDownloadFile_Success()
+    {
+        string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(tempDir);
+
+        string url = "https://www.example.com/index.html";
+        string outputFolder = tempDir;
+
+        await Program.DownloadFileAsync(url, outputFolder);
+
+        string downloadedFilePath = Path.Combine(outputFolder, "index.html");
+
+        Assert.True(File.Exists(downloadedFilePath));
+
+        // Clean up
+        Directory.Delete(tempDir, true);
+    }
+
+    [Fact]
+    public async void TestDownloadFile_InvalidOutputFolder()
+    {
+        string invalidFolder = "invalid-folder"; // Invalid folder path
+        string url = "https://www.example.com/index.html";
+
+        try
+        {
+            await Program.DownloadFileAsync(url, invalidFolder);
+        }
+        catch (IOException ex)
+        {
+            Assert.Contains("Output folder does not exist", ex.Message);
+        }
+    }
+
+    [Fact]
+    public void TestIsValidUrl_ValidUrl()
+    {
+        string validUrl = "https://www.example.com/index.html";
+        bool isValid = Program.IsValidUrl(validUrl);
+        Assert.True(isValid);
+    }
+
+    [Fact]
+    public void TestIsValidUrl_InvalidUrl()
+    {
+        string invalidUrl = "invalid-url"; // Invalid URL
+        bool isValid = Program.IsValidUrl(invalidUrl);
+        Assert.False(isValid);
+    }
+}
---
+++ LR1.6/LR1.6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LR1._6;
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the File Downloader!");
+        Console.Write("Enter the URL of the file you want to download: ");
+        string url = Console.ReadLine();
+
+        Console.Write("Enter the folder where you want to save the file: ");
+        string outputFolder = Console.ReadLine();
+
+        try
+        {
+            if (IsValidUrl(url))
+            {
+                await DownloadFileAsync(url, outputFolder);
+                Console.WriteLine("File downloaded successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid URL.");
+            }
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Invalid input: {ex.Message}");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"IO error: {ex.Message}");
+        }
+        catch (WebException ex)
+        {
+            Console.WriteLine($"Web error: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+
+    public static async Task DownloadFileAsync(string url, string outputFolder)
+    {
+        if (!Directory.Exists(outputFolder))
+        {
+            Directory.CreateDirectory(outputFolder);
+        }
+
+        string fileName = Path.Combine(outputFolder, Path.GetFileName(url));
+
+        using (var client = new WebClient())
+        {
+            await client.DownloadFileTaskAsync(url, fileName);
+        }
+    }
+
+    public static bool IsValidUrl(string url)
+    {
+        // Use a regular expression to validate URL format
+        string urlPattern = @"^(https?://)([a-zA-Z0-9.-]+)(\.[a-zA-Z]{2,6})(:[0-9]+)?(/[a-zA-Z0-9.-]*)*$";
+        return Regex.IsMatch(url, urlPattern);
+    }
+}

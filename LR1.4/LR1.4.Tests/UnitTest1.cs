a08cc06ee25114f550d97d1914103203a51c168d
QuincyClain <pesotskiyvlad9@gmail.com>
2023-10-01 17:10:26 +0300

    task 4 complete

---
+++ LR1.4/LR1.4.Tests/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace LR1._4.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestGenerateHTMLTable_FileExists()
+    {
+        // Arrange & Act
+        Program.Main(null);
+
+        // Assert
+        Assert.True(File.Exists("table.html"));
+    }
+
+    [Fact]
+    public void TestGenerateHTMLTable_CorrectTableStructure()
+    {
+        // Arrange & Act
+        Program.Main(null);
+
+        // Assert
+        string htmlContent = File.ReadAllText("table.html");
+        Assert.Contains("<table>", htmlContent);
+        Assert.Contains("</table>", htmlContent);
+        // Добавьте дополнительные проверки, если необходимо
+    }
+
+    [Fact]
+    public void TestGenerateHTMLTable_BackgroundColorChange()
+    {
+        // Запустите вашу программу, которая создает HTML-файл
+        Program.Main(null);
+
+        // Проверьте, что файл существует
+        Assert.True(File.Exists("table.html"));
+
+        // Прочитайте содержимое файла и убедитесь, что оно содержит ожидаемые строки
+        string htmlContent = File.ReadAllText("table.html", Encoding.UTF8);
+
+        // Проверяем, что фон меняется от светлого к темному
+        Assert.Contains("background-color:rgb(5,5,5);", htmlContent);
+        Assert.Contains("background-color:rgb(30,30,30);", htmlContent);
+        Assert.Contains("background-color:rgb(55,55,55);", htmlContent);
+        Assert.Contains("background-color:rgb(80,80,80);", htmlContent);
+        Assert.Contains("background-color:rgb(105,105,105);", htmlContent);
+        Assert.Contains("background-color:rgb(130,130,130);", htmlContent);
+        Assert.Contains("background-color:rgb(155,155,155);", htmlContent);
+        Assert.Contains("background-color:rgb(180,180,180);", htmlContent);
+        Assert.Contains("background-color:rgb(205,205,205);", htmlContent);
+        Assert.Contains("background-color:rgb(230,230,230);", htmlContent);
+    }
+}
---
+++ LR1.4/LR1.4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+namespace LR1._4;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Создаем HTML-файл и записываем в него заголовок и начало таблицы
+        using (StreamWriter writer = new StreamWriter("table.html"))
+        {
+            writer.WriteLine("<html>");
+            writer.WriteLine("<head>");
+            writer.WriteLine("<meta charset=\"UTF-8\">"); // Кодировка UTF-8
+            writer.WriteLine("<style>");
+            writer.WriteLine("table { border-collapse: collapse; width: 100%; }");
+            writer.WriteLine("td { border: 1px solid black; padding: 8px; }");
+            writer.WriteLine("</style>");
+            writer.WriteLine("</head>");
+            writer.WriteLine("<body>");
+            writer.WriteLine("<table>");
+
+            // Генерируем 10 строк таблицы с изменяющимся фоном
+            for (int i = 1; i <= 10; i++)
+            {
+                string backgroundColor = GetBackgroundColor(i);
+                writer.WriteLine($"<tr style='background-color:{backgroundColor};'>");
+
+                // Здесь можно добавить содержимое ячеек таблицы
+                writer.WriteLine("<td>Ячейка 1</td>");
+                writer.WriteLine("<td>Ячейка 2</td>");
+
+                writer.WriteLine("</tr>");
+            }
+
+            // Завершаем таблицу и HTML-файл
+            writer.WriteLine("</table>");
+            writer.WriteLine("</body>");
+            writer.WriteLine("</html>");
+        }
+
+        Console.WriteLine("HTML-файл с таблицей создан.");
+
+        // Открываем HTML-файл в браузере
+        Process.Start("open", "table.html");
+    }
+
+    // Метод для генерации цвета фона строки таблицы (светлые сверху, темные снизу)
+    static string GetBackgroundColor(int rowIndex)
+    {
+        // Минимальная яркость (черный)
+        int minBrightness = 0;
+
+        // Максимальная яркость (белый)
+        int maxBrightness = 255;
+
+        // Вычисляем яркость для текущей строки (светлые сверху к темным)
+        int brightness = maxBrightness - rowIndex * 25;
+
+        // Формируем цвет фона в формате RGB
+        string color = $"rgb({brightness},{brightness},{brightness})";
+
+        return color;
+    }
+}
+

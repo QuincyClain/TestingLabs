e2f1deacd7bf5e6965e84558d9d361b1cbd4d2d8
QuincyClain <pesotskiyvlad9@gmail.com>
2023-10-01 16:34:48 +0300

    task 2 complete

---
+++ LR1.2/LR1.2.Tests/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using LR1._2;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace LR1._2.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestCalculateStatistics()
+    {
+        // Arrange
+        List<Person> people = new List<Person>
+            {
+                new Person("Иван", "Иванов", 30),
+                new Person("Петр", "Петров", 25),
+                new Person("Анна", "Сидорова", 35)
+            };
+
+        // Act
+        var (minAge, maxAge, averageAge) = Program.CalculateStatistics(people);
+
+        // Assert
+        Assert.Equal(25, minAge);
+        Assert.Equal(35, maxAge);
+        Assert.Equal(30.0, averageAge);
+    }
+
+    [Fact]
+    public void TestCalculateStatistics_OnePerson()
+    {
+        // Arrange
+        List<Person> people = new List<Person>
+    {
+        new Person("Иван", "Иванов", 30)
+    };
+
+        // Act
+        var (minAge, maxAge, averageAge) = Program.CalculateStatistics(people);
+
+        // Assert
+        Assert.Equal(30, minAge);
+        Assert.Equal(30, maxAge);
+        Assert.Equal(30.0, averageAge);
+    }
+
+
+    [Fact]
+    public void TestCalculateStatistics_NegativeAge()
+    {
+        // Arrange
+        List<Person> people = new List<Person>
+            {
+                new Person("Иван", "Иванов", 30),
+                new Person("Петр", "Петров", 25),
+                new Person("Анна", "Сидорова", -5) // Отрицательный возраст
+            };
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => Program.CalculateStatistics(people));
+    }
+
+    [Fact]
+    public void TestCalculateStatistics_SameNames()
+    {
+        // Arrange
+        List<Person> people = new List<Person>
+    {
+        new Person("Иван", "Иванов", 30),
+        new Person("Петр", "Петров", 25),
+        new Person("Иван", "Сидоров", 35) // Человек с одинаковым именем
+    };
+
+        // Act
+        var (minAge, maxAge, averageAge) = Program.CalculateStatistics(people);
+
+        // Assert
+        Assert.Equal(25, minAge); // Минимальный возраст
+        Assert.Equal(35, maxAge); // Максимальный возраст
+        Assert.Equal(30.0, averageAge); // Средний возраст
+    }
+
+    [Fact]
+    public void TestCalculateStatistics_SameNameLastNameAndAge()
+    {
+        // Arrange
+        List<Person> people = new List<Person>
+    {
+        new Person("Иван", "Иванов", 30),
+        new Person("Петр", "Петров", 25),
+        new Person("Иван", "Иванов", 30), // Человек с одинаковым именем, фамилией и возрастом
+        new Person("Анна", "Сидорова", 35)
+    };
+
+        // Act
+        var (minAge, maxAge, averageAge) = Program.CalculateStatistics(people);
+
+        // Assert
+        Assert.Equal(25, minAge); // Минимальный возраст
+        Assert.Equal(35, maxAge); // Максимальный возраст
+        Assert.Equal(30.0, averageAge); // Средний возраст
+    }
+
+}
--- LR1.2/LR1.2/Program.cs
+++ LR1.2/LR1.2/Program.cs
@@ -1,9 +1,87 @@
-﻿namespace LR1._2;
-class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LR1._2;
+public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Person> people = new List<Person>();
+
+        Console.WriteLine("Введите информацию о людях. Для завершения ввода нажмите Enter без ввода данных.");
+
+        while (true)
+        {
+            Console.Write("Введите имя: ");
+            string firstName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(firstName))
+                break;
+
+            Console.Write("Введите фамилию: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Введите возраст: ");
+            if (!int.TryParse(Console.ReadLine(), out int age) || age < 0)
+            {
+                Console.WriteLine("Некорректный возраст. Возраст должен быть положительным числом.");
+                continue;
+            }
+
+            people.Add(new Person(firstName, lastName, age));
+        }
+
+        if (people.Any())
+        {
+            Console.WriteLine("\nСписок людей:");
+            foreach (var person in people)
+            {
+                Console.WriteLine($"{person.LastName} {person.FirstName} {person.Age}");
+            }
+
+            var (minAge, maxAge, averageAge) = CalculateStatistics(people);
+
+            Console.WriteLine($"\nСамый маленький возраст: {minAge}");
+            Console.WriteLine($"Самый большой возраст: {maxAge}");
+            Console.WriteLine($"Средний возраст: {averageAge:F2}");
+        }
+        else
+        {
+            Console.WriteLine("Список людей пуст.");
+        }
+    }
+
+    public static (int minAge, int maxAge, double averageAge) CalculateStatistics(List<Person> people)
+    {
+        if (!people.Any())
+        {
+            return (int.MaxValue, int.MinValue, double.NaN);
+        }
+
+        if (people.Any(p => p.Age < 0))
+        {
+            throw new InvalidOperationException("Отрицательный возраст не допускается.");
+        }
+
+        int minAge = people.Min(p => p.Age);
+        int maxAge = people.Max(p => p.Age);
+        double averageAge = people.Average(p => p.Age);
+        return (minAge, maxAge, averageAge);
+    }
+}
+
+public class Person
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public int Age { get; }
+
+    public Person(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
     }
 }
 
